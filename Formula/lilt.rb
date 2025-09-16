3e9ee30035871692698b282df30959aa3e89a054
Arda Kilicdagi <ardakilicdagi@gmail.com>
2025-09-16 21:45:23 +0400

    feature: Brew tap formula

---
+++ Formula/lilt.rb
@@ -0,0 +1,23 @@
+class Lilt < Formula
+  desc "A CLI tool for transcoding HiFi music files to 16bit variations"
+  homepage "https://github.com/Ardakilic/lilt"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/Ardakilic/lilt/releases/download/v3.1.0/lilt-darwin-arm64.tar.gz"
+      sha256 "09c8d1124c807d9a2eeae3741255c82b61ba9a32f7082473af8cedf7f50216b9"
+    else
+      url "https://github.com/Ardakilic/lilt/releases/download/v3.1.0/lilt-darwin-amd64.tar.gz"
+      sha256 "dedb15d950da0a943d2fa116c04292a2dd80f5deeda22e1a4a8a5cee179d8002"
+    end
+  end
+
+  def install
+    bin.install "lilt"
+  end
+
+  test do
+    system "#{bin}/lilt", "--version"
+  end
+end
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # homebrew-lilt
 Lilt: A tool written with Golang that converts HiFi Flac files on a folder recursively
+
+To install Lilt using [Homebrew](https://brew.sh), simply execute:
+
+```bash
+brew tap Ardakilic/lilt
+brew install lilt
+```
+
+https://github.com/Ardakilic/lilt
