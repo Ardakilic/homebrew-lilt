abbff4db3615820b06131468d82f43b0a25cc275
Arda Kilicdagi <ardakilicdagi@gmail.com>
2025-09-16 23:26:25 +0400

    chore: fix archive hashes

--- Formula/lilt.rb
+++ Formula/lilt.rb
@@ -6,10 +6,10 @@ class Lilt < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Ardakilic/lilt/releases/download/v3.1.0/lilt-darwin-arm64.tar.gz"
-      sha256 "09c8d1124c807d9a2eeae3741255c82b61ba9a32f7082473af8cedf7f50216b9"
+      sha256 "dedb15d950da0a943d2fa116c04292a2dd80f5deeda22e1a4a8a5cee179d8002"
     else
       url "https://github.com/Ardakilic/lilt/releases/download/v3.1.0/lilt-darwin-amd64.tar.gz"
-      sha256 "dedb15d950da0a943d2fa116c04292a2dd80f5deeda22e1a4a8a5cee179d8002"
+      sha256 "09c8d1124c807d9a2eeae3741255c82b61ba9a32f7082473af8cedf7f50216b9"
     end
   end
 
